92e51734bf7f216404811f543e6c9bcd51dc76c5
Shahan Dhanuddara <65756207+shehandhanu@users.noreply.github.com>
2022-11-16 17:02:26 +0530

    data base changed

--- src/configs/db.conn.js
+++ src/configs/db.conn.js
@@ -4,18 +4,13 @@ import path from "path";
 import { logger } from "../utils";
 
 let database;
-const certificate = path.join(__dirname, "..", "..", "X509-cert-87529051924629561.pem");
+// const certificate = path.join(__dirname, "..", "..", "X509-cert-87529051924629561.pem");
 
 export const dbConnect = () => {
   if (database) return;
 
   mongoose
-    .connect(process.env.MONGODB_URI, {
-      ssl: true,
-      sslKey: certificate,
-      sslCert: certificate,
-      serverApi: ServerApiVersion.v1,
-    })
+    .connect(process.env.MONGOURL)
     .then((connection) => {
       database = connection.connection;
       logger.info(`Database synced`);
